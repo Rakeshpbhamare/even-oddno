f62d536d48095525637867e14c24c105309e6c34
Rakeshpbhamare <113248605+Rakeshpbhamare@users.noreply.github.com>
2022-09-21 11:49:44 +0530

    Add files via upload

---
+++ even or odd.py
@@ -0,0 +1,23 @@
+
+'''
+while True:
+    n=int(input('enter no'))
+    if n%2==0:
+        print(n , "is even no")
+    else:
+        print(n ,"is odd no")
+
+'''
+from telnetlib import PRAGMA_HEARTBEAT
+
+
+n=int(input('no'))
+for i in range(1,n):
+    if n%2==0:
+        print(n,"is even no")
+        if n%5==0:
+            print(n,"is divided by 5")
+            if n+5==20:
+                break
+    else:
+        print('wrong no')
